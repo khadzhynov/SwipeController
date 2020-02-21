62c8e652a69ca33ce4ab7ae65abaaefdd0309f0d
Mykhailo Khadzhynov <bryarey@gmail.com>
2020-02-21 14:26:37 +0300

    Added documentation. minor fixes.

--- Assets/SwipeController/Scripts/Examples/MoveOnSwipe_Vector.cs
+++ Assets/SwipeController/Scripts/Examples/MoveOnSwipe_Vector.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MoveOnSwipe_Vector : MonoBehaviour
 {
 
--- Assets/SwipeController/Scripts/SwipesToVectorEvents.cs
+++ Assets/SwipeController/Scripts/SwipesToVectorEvents.cs
@@ -4,63 +4,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[Serializable]
-public class Vector3Event
+namespace GG.Infrastructure.Utils.Swipe
 {
-    public Vector3 Vector;
-    public SwipeVectorEvent Event;
-}
 
-[Serializable]
-public class SwipeVectorEvent : UnityEvent<Vector3> { }
-
-public class SwipesToVectorEvents : MonoBehaviour
-{
-    private const string ID_PREFIX = "customID_{0}";
-
-    [SerializeField]
-    private List<Vector3Event> _swipes;
-
-    [SerializeField]
-    private SwipeListener _swipeListener;
-
-    private Dictionary<string, Vector3Event> _swipesDictionary;
-
-    private void Start()
+    [Serializable]
+    public class Vector3Event
     {
-        List<DirectionId> directions = new List<DirectionId>();
+        public Vector3 Vector;
+        public SwipeVectorEvent Event;
+    }
 
-        _swipesDictionary = new Dictionary<string, Vector3Event>();
+    [Serializable]
+    public class SwipeVectorEvent : UnityEvent<Vector3> { }
 
-        foreach (var swipe in _swipes)
+    public class SwipesToVectorEvents : MonoBehaviour
+    {
+        private const string ID_PREFIX = "customID_{0}";
+
+        [SerializeField]
+        private List<Vector3Event> _swipes;
+
+        [SerializeField]
+        private SwipeListener _swipeListener;
+
+        private Dictionary<string, Vector3Event> _swipesDictionary;
+
+        private void Start()
         {
-            string id = string.Format(ID_PREFIX, swipe.Vector.ToString());
-            directions.Add(new DirectionId(id, swipe.Vector));
-            _swipesDictionary.Add(id, swipe);
+            List<DirectionId> directions = new List<DirectionId>();
+
+            _swipesDictionary = new Dictionary<string, Vector3Event>();
+
+            foreach (var swipe in _swipes)
+            {
+                string id = string.Format(ID_PREFIX, swipe.Vector.ToString());
+                directions.Add(new DirectionId(id, swipe.Vector));
+                _swipesDictionary.Add(id, swipe);
+            }
+            _swipeListener.SwipeDetectionMode = SwipeDetectionMode.Custom;
+            _swipeListener.SetDetectionMode(directions);
         }
-        _swipeListener.SwipeDetectionMode = SwipeDetectionMode.Custom;
-        _swipeListener.SetDetectionMode(directions);
-    }
 
-    private void OnEnable()
-    {
-        _swipeListener.OnSwipe.AddListener(OnSwipeHandler);
-    }
-
-    private void OnDisable()
-    {
-        _swipeListener.OnSwipe.RemoveListener(OnSwipeHandler);
-    }
-
-    private void OnSwipeHandler(string id)
-    {
-        if (_swipesDictionary.ContainsKey(id))
+        private void OnEnable()
         {
-            _swipesDictionary[id].Event?.Invoke(_swipesDictionary[id].Vector);
+            _swipeListener.OnSwipe.AddListener(OnSwipeHandler);
         }
-        else
+
+        private void OnDisable()
         {
-            Debug.LogWarning("Unrecognized swipe id!");
+            _swipeListener.OnSwipe.RemoveListener(OnSwipeHandler);
+        }
+
+        private void OnSwipeHandler(string id)
+        {
+            if (_swipesDictionary.ContainsKey(id))
+            {
+                _swipesDictionary[id].Event?.Invoke(_swipesDictionary[id].Vector);
+            }
+            else
+            {
+                Debug.LogWarning("Unrecognized swipe id!");
+            }
         }
     }
 }
